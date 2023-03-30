442b46b52dfbc2705c327e13cd76fe53cb0c0917
HARRY OMOLLOH <hthuku95@gmail.com>
2023-03-30 23:42:34 +0300

    created scripts

---
+++ scripts/deploy.py
@@ -0,0 +1,11 @@
+from brownie import SneakerNFT, network, config
+from scripts.helpful_scripts import get_account
+from web3 import Web3
+
+def main():
+    account = get_account()
+    sneaker_nft = SneakerNFT.deploy(
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False)
+    )
+    print(sneaker_nft.name())
